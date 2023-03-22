67d099061c10c5708ac9e26fce9331c73d5f18f1
Lara-Vel <lara.vel@outlook.com>
2023-03-22 13:43:10 +0100

    import dotenv

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -1,5 +1,7 @@
 import bunyan from 'bunyan';
 import log from './logger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 describe('Given a logger', () => {
   test('When a logger is created, then it should be an instance of a bunyan logger', () => {
