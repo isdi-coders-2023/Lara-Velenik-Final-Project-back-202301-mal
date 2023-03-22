8f672ff74c39ad5b8fd1deb2f9bf9afb898a1373
Lara-Vel <lara.vel@outlook.com>
2023-03-22 17:24:59 +0100

    add router of get and get by id

--- src/api/ads/ads-router.ts
+++ src/api/ads/ads-router.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createAdController } from './ads-controller.js';
+import {
+  createAdController,
+  getAdByIdController,
+  getAllAdsController,
+} from './ads-controller.js';
 import { upload } from './picture-upload.middleware.js';
 
 const router = express.Router();
 
-// Router.route('/').get(getAdsController).post(createAdController);
+router.route('/').get(getAllAdsController);
+
+router.route('/:_id').get(getAdByIdController);
 
 router.route('/create').post(upload.single('cat-ad'), createAdController);
 
