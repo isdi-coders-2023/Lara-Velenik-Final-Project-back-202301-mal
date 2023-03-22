2189c82dc12365f8a827accbc906df12d1571fe6
Lara-Vel <lara.vel@outlook.com>
2023-03-22 13:37:12 +0100

    add authMiddleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import apiRouter from './api/api-router.js';
 import authRouter from './api/auth/auth-router.js';
 import { errorHandler } from './utils/error-handler.js';
 import dotenv from 'dotenv';
+import { authMiddleware } from './api/auth/auth-middleware.js';
 dotenv.config();
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
-app.use('/api/v1', apiRouter);
+app.use('/api/v1', authMiddleware, apiRouter);
 
 app.use(bodyParser.json());
 
